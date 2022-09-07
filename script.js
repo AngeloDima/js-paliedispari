8259747b2ab04213a0f041f5dca4e0523e3e99a2
Angelo <Angelodimauro91@gmail.com>
2022-09-07 10:07:30 +0200

    chiedo una parola all'utente, e cerco di capire se la parola è palindrome

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-const word = prompt('Damm una parola');
+
+
+// Chiedo all’utente di inserire una parola
+
+const word = prompt('Dammi una parola');
 
 const isPalindroma = checkPalindroma(word);
 
 console.log('è palindroma:', isPalindroma)
+
+
+
+
+
+
+
+    // funzione per capire se la parola è palindrome
+
+
+
+function checkPalindroma(text){
+    
+    let reversedText = '';
+    // ciclo i caratteri di text
+     console.log('versione 1');
+
+    for(let i = 0; i < text.length; i++){
+        const char = text.charAt(i);
+        
+        reversedText = char + reversedText; 
+        console.log(reversedText);
+    }
+
+
+
+
+    console.log('reversedText', reversedText);
+    // controllo se il testo risultante è uguale a quello di input
+    const isEqual = reversedText === text;
+    // ritorno vero se è palindroma altrimenti è falso
+    return isEqual;
+
+}

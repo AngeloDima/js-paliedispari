17bf9d3a537e0a68aa58d56d0b0304e70bdc7434
Angelo <Angelodimauro91@gmail.com>
2022-09-07 10:10:22 +0200

    comment

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 
 
-// Chiedo all’utente di inserire una parola
+// // Chiedo all’utente di inserire una parola
 
-const word = prompt('Dammi una parola');
+// const word = prompt('Dammi una parola');
 
-const isPalindroma = checkPalindroma(word);
+// const isPalindroma = checkPalindroma(word);
 
-console.log('è palindroma:', isPalindroma)
-
-
+// console.log('è palindroma:', isPalindroma)
 
 
 
 
 
-    // funzione per capire se la parola è palindrome
+
+
+//     // funzione per capire se la parola è palindrome
 
 
 
-function checkPalindroma(text){
+// function checkPalindroma(text){
     
-    let reversedText = '';
-    // ciclo i caratteri di text
-     console.log('versione 1');
+//     let reversedText = '';
+//     // ciclo i caratteri di text
+//      console.log('versione 1');
 
-    for(let i = 0; i < text.length; i++){
-        const char = text.charAt(i);
+//     for(let i = 0; i < text.length; i++){
+//         const char = text.charAt(i);
         
-        reversedText = char + reversedText; 
-        console.log(reversedText);
-    }
+//         reversedText = char + reversedText; 
+//         console.log(reversedText);
+//     }
 
 
 
 
-    console.log('reversedText', reversedText);
-    // controllo se il testo risultante è uguale a quello di input
-    const isEqual = reversedText === text;
-    // ritorno vero se è palindroma altrimenti è falso
-    return isEqual;
+//     console.log('reversedText', reversedText);
+//     // controllo se il testo risultante è uguale a quello di input
+//     const isEqual = reversedText === text;
+//     // ritorno vero se è palindroma altrimenti è falso
+//     return isEqual;
 
-}
+// }
 
 
 
